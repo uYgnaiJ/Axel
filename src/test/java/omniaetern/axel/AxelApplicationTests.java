525a6734c6ba1c291405cc8bf25f1ee16ff1293c
YU JIANG <60321254+uYgnaiJ@users.noreply.github.com>
2025-12-02 14:50:10 +0800

    init springboot

---
+++ src/test/java/omniaetern/axel/AxelApplicationTests.java
@@ -0,0 +1,13 @@
+package omniaetern.axel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
